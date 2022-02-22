165b95ed8e3842405fd71f9d099a9e495a5c1f66
wemmelotec <tonmelo@hotmail.com>
2022-02-22 14:57:49 -0300

    end class 9.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { BemVindoComponent } from './bem-vindo/bem-vindo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    BemVindoComponent
   ],
   imports: [
     BrowserModule
