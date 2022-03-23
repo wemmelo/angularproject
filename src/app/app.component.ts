7cbc83b691346a06b47e7e8a7273d49eee329c07
wemmelotec <tonmelo@hotmail.com>
2022-03-23 17:20:31 -0300

    data bindig interpolação and event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularproject';
+  data = 2022;
+  nome = "Wellington";
+
+  //criei este método para ser utilizado no data binding com interpolação
+  getData(){
+    return this.data;
+  }
+
+  //este método vai ser utilizado no data binding event binding
+  adicionar(){
+    console.log(`Adicionando  ${this.nome}`)
+
+    const numero = Math.round(Math.random() * 100);
+
+    this.nome = `João` + numero;
+  }
+
+  alterarNome(event: any){
+      //console.log(event);
+      this.nome = event.target.value;
+  }
 }
